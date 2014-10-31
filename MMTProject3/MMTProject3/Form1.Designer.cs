dfcf182056449a047946bf6281aa2ea645834057
Pieter De Bruyne <pieter.debruyne@kahosl.be>
2014-10-31 14:04:58 +0100

    to github

---
+++ MMTProject3/MMTProject3/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace MMTProject3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.obstacleRadioButton = new System.Windows.Forms.RadioButton();
+            this.startRadioButton = new System.Windows.Forms.RadioButton();
+            this.endRadioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(527, 359);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(545, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Nieuw Speelveld";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // obstacleRadioButton
+            // 
+            this.obstacleRadioButton.AutoSize = true;
+            this.obstacleRadioButton.Location = new System.Drawing.Point(6, 65);
+            this.obstacleRadioButton.Name = "obstacleRadioButton";
+            this.obstacleRadioButton.Size = new System.Drawing.Size(67, 17);
+            this.obstacleRadioButton.TabIndex = 4;
+            this.obstacleRadioButton.TabStop = true;
+            this.obstacleRadioButton.Text = "Obstakel";
+            this.obstacleRadioButton.UseVisualStyleBackColor = true;
+            this.obstacleRadioButton.CheckedChanged += new System.EventHandler(this.obstacleRadioButton_CheckedChanged);
+            // 
+            // startRadioButton
+            // 
+            this.startRadioButton.AutoSize = true;
+            this.startRadioButton.Location = new System.Drawing.Point(6, 19);
+            this.startRadioButton.Name = "startRadioButton";
+            this.startRadioButton.Size = new System.Drawing.Size(47, 17);
+            this.startRadioButton.TabIndex = 2;
+            this.startRadioButton.TabStop = true;
+            this.startRadioButton.Text = "Start";
+            this.startRadioButton.UseVisualStyleBackColor = true;
+            this.startRadioButton.CheckedChanged += new System.EventHandler(this.startRadioButton_CheckedChanged);
+            // 
+            // endRadioButton
+            // 
+            this.endRadioButton.AutoSize = true;
+            this.endRadioButton.Location = new System.Drawing.Point(6, 42);
+            this.endRadioButton.Name = "endRadioButton";
+            this.endRadioButton.Size = new System.Drawing.Size(52, 17);
+            this.endRadioButton.TabIndex = 3;
+            this.endRadioButton.TabStop = true;
+            this.endRadioButton.Text = "Einde";
+            this.endRadioButton.UseVisualStyleBackColor = true;
+            this.endRadioButton.CheckedChanged += new System.EventHandler(this.endRadioButton_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.startRadioButton);
+            this.groupBox1.Controls.Add(this.obstacleRadioButton);
+            this.groupBox1.Controls.Add(this.endRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(545, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(120, 94);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tekenmodus";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(545, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Zoek Pad";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(545, 170);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Diagonaal";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(545, 193);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(130, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Euclidische Heuristiek";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(545, 241);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(549, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Padlengte";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 383);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton obstacleRadioButton;
+        private System.Windows.Forms.RadioButton startRadioButton;
+        private System.Windows.Forms.RadioButton endRadioButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MMTProject3/MMTProject3/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MMTProject3
+{
+    public partial class Form1 : Form
+    {
+        private PathFinder p;
+        private Graphics pbGraphics;
+        private Graphics bufferGraphics;
+        private Bitmap backbuffer;
+        private int nrows;
+        private int ncolumns;
+        private int cellSize;
+        private bool hasStart = false;
+        private bool hasEnd = false;
+        private CellType drawMode;
+        private bool mousedown = false;
+        private int lastCellX = 0;
+        private int lastCellY = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            backbuffer = new Bitmap(panel1.Width,panel1.Height);
+            nrows = 17;
+            ncolumns = 26;
+            cellSize = 20;
+            pbGraphics = panel1.CreateGraphics();
+            bufferGraphics = Graphics.FromImage(backbuffer);
+            bufferGraphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            p = new PathFinder(nrows,ncolumns,bufferGraphics,cellSize);
+            drawMode = CellType.obstacle;
+            obstacleRadioButton.Select();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p = new PathFinder(nrows, ncolumns, bufferGraphics, cellSize);
+            textBox1.Text = "";
+            Redraw();
+        }
+
+        private void Redraw()
+        {
+            bufferGraphics.Clear(Color.MintCream);
+            p.drawField();
+            p.drawCells();
+            pbGraphics.DrawImage(backbuffer, new Rectangle(0, 0, panel1.Width, panel1.Height), new Rectangle(0, 0, panel1.Width, panel1.Height), GraphicsUnit.Pixel);
+
+        }
+
+        private void DoClickAndDrag(MouseEventArgs e)
+        {
+            Console.WriteLine("x:" + e.X);
+            Console.WriteLine("y:" + e.Y);
+
+            if (e.X >= ncolumns * cellSize || e.X < 0 || e.Y >= nrows * cellSize || e.Y < 0) return;
+
+            int column = e.X / cellSize;
+            int row = e.Y / cellSize;
+            lastCellX = column;
+            lastCellY = row;
+            CellType original = p.GetCellType(row, column);
+
+            p.setCelltype(row, column, drawMode, original);
+            Redraw();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousedown = true;
+            DoClickAndDrag(e);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int currentCellX = e.X/cellSize;
+            int currentCellY = e.Y/cellSize;
+            if (mousedown && !(currentCellX == lastCellX && currentCellY == lastCellY) && drawMode == CellType.obstacle)
+            {
+                DoClickAndDrag(e);
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mousedown = false;
+        }
+
+        private void startRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            drawMode = CellType.start;
+        }
+
+        private void endRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            drawMode = CellType.end;
+        }
+
+        private void obstacleRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            drawMode = CellType.obstacle;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (p.FindPath())
+            {
+                Console.WriteLine("pad gevonden");
+                p.SetPath();
+                Redraw();
+                textBox1.Text = p.PadLengte.ToString();
+            }
+            else
+            {
+                Console.WriteLine("geen pad gevonden");
+                textBox1.Text = "geen pad";
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.CheckState == CheckState.Checked)
+            {
+                p.AllowDiagonal = true;
+            }
+            else if (checkBox1.CheckState == CheckState.Unchecked)
+            {
+                p.AllowDiagonal = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.CheckState == CheckState.Checked)
+            {
+                p.Euclidean = true;
+            }
+            else if (checkBox2.CheckState == CheckState.Unchecked)
+            {
+                p.Euclidean = false;
+            }
+        }
+
+    }
+}
